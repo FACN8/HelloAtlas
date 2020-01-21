a074fac796d098b407205f8e5660e9c9b6a59abc
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-01-21 16:00:44 +0200

    Adding dotenv and server #2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-
 const http = require('http');
 const router = require('./src/router');
 const server = http.createServer(router);
 
-server.listen(5050 , () => {
+server.listen(SERV, () => {
     console.log('Server is listening on port 5050,armed and ready!');
 
 });
