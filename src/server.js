978a7691cf8b97c100dc2cc1c71527eab20ac99b
Rabea Fahoum <Rabea.fahoum@gmail.com>
2020-01-22 15:41:45 +0200

    heroku

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const http = require('http');
 const server = http.createServer(router);
 require('dotenv').config();
 
-server.listen(8080, () => {
+server.listen(process.env.PORT || 8080, () => {
     console.log('Server is listening on port 8080,armed and ready!');
 });
